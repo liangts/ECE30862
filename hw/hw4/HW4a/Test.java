10ab04c8280e18bdb8c3c832febe112eef69299b
Soalogy <soalogy@gmail.com>
2016-09-12 22:24:01 -0400

    HW4a
    hw4a

---
+++ hw/hw4/HW4a/Test.java
@@ -0,0 +1,21 @@
+package HW4a;
+
+// This is the main class for HW4a
+// This program is filled with compile errors.
+public class Test {
+
+   public static void main(String[] args) {
+      if (args.length != 0) {
+         System.out.println("Usage: java Main");
+         return;
+
+      }
+
+      Base b = new Base( );
+      Derived d = new Derived( );
+
+      b.f1( ); 
+      d.f2( ); 
+    }
+}
+
