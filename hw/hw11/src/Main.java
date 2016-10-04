b08f0ab6eb374379c178040b9bfb4c3551d8e83b
Tianshu Liang <liangts0423@gmail.com>
2016-10-04 16:12:07 -0400

    HW11 0.1 Need add test statements

--- hw/hw11/src/Main.java
+++ hw/hw11/src/Main.java
@@ -1,13 +1,71 @@
-/**
- * Created by liang on 2016/10/3.
- */
 public class Main {
-    public static void main(String args[]){
-        int N = 5;
-        matrix A = new matrix(N);
-        matrix B = new matrix(N);
-        matrix C = new matrix(N);
+    public int[] _get_matrix_size(int[][] arr_2d){
+        int[] return_array_size = new int[2];
+        return_array_size[0] = arr_2d.length;
+        return_array_size[1] = arr_2d[0].length;
 
+        return return_array_size;
+    }
+
+    public void fill(int N){
+        if (N % 2 != 0){
+            throw new IllegalArgumentException("N % 2 should == 0!");
+        //Need to fill
+        }
+    }
+
+    public void _print_matrix(int[][] matrix){
+        int n = matrix.length;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println("");
+        }
 
     }
+
+    public void _thread_calc(int[][] A, int[][] B, int[][] C, int x1, int x2, int y1, int y2){
+        class _thread implements Runnable{
+            public void run(){
+                for (int i = x1; i < x2; i++){
+                    for (int j = y1; j < y2; j++){
+                        A[i][j] = B[i][j] * C[i][j];
+                    }
+                }
+            }
+        }
+        Thread t = new Thread((new _thread()));
+        t.start();
+    }
+
+    public int[][] _calc_array_A(int[][] B, int[][] C, int thread_num){
+        int N = B.length;
+        int[][] A = new int[N][N];
+
+        if (thread_num == 1){
+            _thread_calc(A, B, C, 0, N, 0, N);
+        }
+
+        if (thread_num == 2){
+            _thread_calc(A, B, C, 0, N / 2, 0, N);
+            _thread_calc(A, B, C, N / 2, N, 0, N);
+        }
+
+        if (thread_num == 4){
+            _thread_calc(A, B, C, 0, N / 2, 0, N / 2);
+            _thread_calc(A, B, C, 0, N / 2, N / 2, N);
+            _thread_calc(A, B, C, N / 2, N, 0, N / 2);
+            _thread_calc(A, B, C, N / 2, N, N / 2, N);
+
+        }
+
+        return A;
+    }
+
+    public static void main(String args[]){
+
+    }
+
+
 }
